fed5ba3da7aabec6e49e728757a8a06ec49187e3
Lucas Trindade <codebylucas@gmail.com>
2026-06-27 22:58:52 -0300

    update: using .add to add operators

--- src/Main.java
+++ src/Main.java
@@ -4,14 +4,17 @@ import domain.Soma;
 import domain.Subtracao;
 import presentation.Menu;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        List<OperacaoItem> operacoes = List.of(
-            new OperacaoItem("Soma", new Soma()),
-            new OperacaoItem("Subtração", new Subtracao())
-        );
+        List<OperacaoItem> operacoes = new ArrayList<>();
+
+        // Serviços
+        operacoes.add(new OperacaoItem("Soma", new Soma()));
+        operacoes.add(new OperacaoItem("Subtração", new Subtracao()));
+
         CalculadoraService servico = new CalculadoraService(operacoes);
         Menu menu = new Menu(servico);
 
